013bee2971d24e19ecbfb178cc50df334d247e6d
IsaoOhnuki <isa0_net@hotmail.com>
2023-04-30 10:25:39 +0900

    commit

---
+++ CPP/CppLib/AxLib/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h : モジュール クラスの宣言です。
+
+class CAxLibModule : public ATL::CAtlDllModuleT< CAxLibModule >
+{
+public :
+	DECLARE_LIBID(LIBID_AxLibLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_AXLIB, "{0130d584-c3c4-45a6-a0bf-8bad22747ef3}")
+};
+
+extern class CAxLibModule _AtlModule;
