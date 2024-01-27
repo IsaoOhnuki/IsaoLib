ae51abba4807999d1677f38bb1a6bf3fa177038f
IsaoOhnuki <isa0_net@hotmail.com>
2024-01-27 10:30:06 +0900

    commit

--- CSharp/NetFramework/GUIDMaker/Form1.Designer.cs
+++ CSharp/NetFramework/GUIDMaker/Form1.Designer.cs
@@ -28,80 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // tabControl1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Header";
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(491, 394);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(483, 366);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "GUID";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(3, 27);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(477, 336);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.WordWrap = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.MinimumSize = new System.Drawing.Size(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(477, 24);
+            this.panel1.TabIndex = 9;
             // 
             // textBox1
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(191, 23);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(353, 23);
+            this.textBox1.TabIndex = 5;
             this.textBox1.Text = "0130";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(353, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 24);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(210, 30);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(415, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 23);
-            this.button1.TabIndex = 2;
+            this.button1.Size = new System.Drawing.Size(62, 24);
+            this.button1.TabIndex = 6;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // tabPage2
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(12, 60);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(275, 207);
-            this.textBox2.TabIndex = 3;
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(483, 366);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "COM";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.MinimumSize = new System.Drawing.Size(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(477, 24);
+            this.panel2.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(3, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(477, 336);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.Location = new System.Drawing.Point(402, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 24);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(299, 279);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(491, 394);
+            this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- CSharp/NetFramework/GUIDMaker/Form1.cs
+++ CSharp/NetFramework/GUIDMaker/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -35,7 +36,95 @@ namespace GUIDMaker
                     i++;
                 }
             }
-            textBox2.Text = sb.ToString();
+            textBox2.Text += sb.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length > 0)
+            {
+                Clipboard.SetText(textBox2.Text);
+            }
+        }
+
+        [Flags]
+        enum MSIINSTALLCONTEXT
+        {
+            MSIINSTALLCONTEXT_NONE = 0,
+            MSIINSTALLCONTEXT_USERMANAGED = 1,
+            MSIINSTALLCONTEXT_USERUNMANAGED = 2,
+            MSIINSTALLCONTEXT_MACHINE = 4,
+            MSIINSTALLCONTEXT_ALL = MSIINSTALLCONTEXT_USERMANAGED | MSIINSTALLCONTEXT_USERUNMANAGED | MSIINSTALLCONTEXT_MACHINE,
+        }
+
+        enum MSIGETERROR
+        {
+            ERROR_SUCCESS = 0,
+            ERROR_ACCESS_DENIED = 5,
+            ERROR_INVALID_PARAMETER = 87,
+            ERROR_MORE_DATA = 234,
+            ERROR_NO_MORE_ITEMS = 259,
+            ERROR_BAD_CONFIGURATION = 1610,
+            ERROR_FUNCTION_FAILED = 1627,
+
+            INSTALLSTATE_NOTUSED,
+            INSTALLSTATE_BADCONFIG,
+            INSTALLSTATE_ABSENT,
+            INSTALLSTATE_INVALIDARG,
+            INSTALLSTATE_LOCAL,
+            INSTALLSTATE_SOURCE,
+            INSTALLSTATE_SOURCEABSENT,
+            INSTALLSTATE_UNKNOWN,
+            INSTALLSTATE_BROKEN,
+        }
+
+        static string Everyone = "s-1-1-0";
+
+        [DllImport("Msi.dll")]
+        static extern uint MsiEnumClientsExW(
+            string szComponent,
+            string szUserSid,
+            MSIINSTALLCONTEXT dwContext,
+            uint dwProductIndex,
+            out string szInstalledComponentCode,
+            out MSIINSTALLCONTEXT pdwInstalledContext,
+            out string szSid,
+            ref uint pcchSid);
+
+        [DllImport("Msi.dll")]
+        static extern uint MsiEnumComponentsExW(
+            string szUserSid,
+            MSIINSTALLCONTEXT dwContext,
+            uint dwIndex,
+            out string szInstalledComponentCode,
+            out MSIINSTALLCONTEXT pdwInstalledContext,
+            out string szSid,
+            ref UIntPtr pcchSid);
+
+        [DllImport("Msi.dll")]
+        static extern uint MsiGetComponentPathExW(string szProductCode, string szComponentCode, string szUserSid,
+            MSIINSTALLCONTEXT dwContext,
+            out string lpOutPathBuffer,
+            ref string pcchOutPathBuffer);
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UIntPtr pcchSid = UIntPtr.Zero;
+            for (uint i = 0; i < uint.MaxValue; i++)
+            {
+                uint ret = MsiEnumComponentsExW(
+                    Everyone,
+                    MSIINSTALLCONTEXT.MSIINSTALLCONTEXT_ALL,
+                    i,
+                    out string szInstalledComponentCode,
+                    out MSIINSTALLCONTEXT pdwInstalledContext,
+                    out string szSid,
+                    ref pcchSid);
+                if (ret != (uint)MSIGETERROR.ERROR_SUCCESS)
+                {
+                    break;
+                }
+            }
         }
     }
 }
